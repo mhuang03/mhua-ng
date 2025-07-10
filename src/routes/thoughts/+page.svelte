e06e6e2de9deda5fa73ea9a78c04f2f805c9b13a
mhuang03 <huangmichael150@gmail.com>
2025-07-10 14:54:05 -0500

    added search bar to posts

--- src/routes/thoughts/+page.svelte
+++ src/routes/thoughts/+page.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
   import TagFilter from "$lib/components/TagFilter.svelte";
   import PostRow from "$lib/components/PostRow.svelte";
+  import SearchIcon from "~icons/mynaui/search";
   import type { Post } from "$lib/types.js";
 
   let { data } = $props();
   let selected = $state(new Set<string>());
+  let searchQuery = $state("");
 
   let filteredPosts = $derived.by(() =>
     data.posts.filter((post: Post) => {
-      if (selected.size === 0) return true;
-      return Array.from(selected).every((tag) => post.tags && post.tags.includes(tag));
+      const matchTags =
+        selected.size === 0 ? true : Array.from(selected).every((tag) => post.tags && post.tags.includes(tag));
+      const matchSearch =
+        post.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        post.description.toLowerCase().includes(searchQuery.toLowerCase());
+      return matchTags && matchSearch;
     })
   );
 </script>
@@ -20,8 +26,12 @@
 
 Musings of a XXI-century thinker.
 <ul class="list bg-base-100 rounded-box shadow-md max-w-2xl mx-auto">
-  <div class="list-row rounded-b-none gap-0">
+  <div class="list-row p-5 rounded-box flex flex-col-reverse sm:flex-row justify-between gap-2 sm:gap-5">
     <TagFilter tags={data.tags} bind:selected />
+    <label class="input w-full sm:max-w-50 sm:input-xs">
+      <SearchIcon class="h-[1em] opacity-50" />
+      <input type="search" name="search" required placeholder="Search" bind:value={searchQuery} />
+    </label>
   </div>
   {#each filteredPosts as post}
     <PostRow {post} />
